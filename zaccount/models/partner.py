f41d486fd1b6f57104f5f752ed3f5ae49ad6c2ee
dargombesjy <dargombesjy@gmail.com>
2020-11-03 04:51:11 +0700

    commit_03112020_01

--- zaccount/models/partner.py
+++ zaccount/models/partner.py
@@ -10,3 +10,10 @@ class ResPartner(models.Model):
     wht_tax = fields.Many2one('account.tax', string="Withholding Tax")
         # fields.Many2many('account.tax', 'service_operation_line_tax', 'service_operation_line_id', 'tax_id', 'WHT Taxes')
     wht_proportion = fields.Float('WHT proportion', default=1.0)
+
+    @api.multi
+    def write(self, vals):
+        if vals['wht_proportion'] > 1.0:
+            raise UserError(_('WHT Proportion dalam desimal harus kurang dari atau sama dengan 1'))
+        res = super(ResPartner, self).write(vals)
+        return res
