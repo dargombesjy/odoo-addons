f16969b408d8132814099b1a684984127bfa97d3
dargombesjy <dargombesjy@gmail.com>
2021-02-09 05:08:31 +0700

    commit_09022021_02

--- zdg_product/models/product.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-    
-    make_id = fields.Many2one('service.equipment.make', 'Manufacturer')
-    model_id = fields.Many2one('service.equipment.variant', 'Model')
-    generic_name = fields.Char('Nama umum')
-    note = fields.Char('Keterangan')
